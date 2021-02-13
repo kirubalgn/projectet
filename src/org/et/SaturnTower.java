6ee58f76aabdb14a3096f8a5e5a88b2560250618
kirubalgn <kirubalgn@gmail.com>
2021-02-13 15:08:44 +0530

    program executed

---
+++ src/org/et/SaturnTower.java
@@ -0,0 +1,18 @@
+package org.et;
+
+public class SaturnTower {
+	
+	public void ring() {
+		System.out.println("Fire on");
+
+	}
+
+	public static void main(String[] args) {
+		
+		SaturnTower s= new SaturnTower();
+		s.ring();
+				
+
+	}
+
+}
